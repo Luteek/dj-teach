5cb48532440fc887904d7be3d2228aabddf34851
luteek_dev <develop.luteek@gmail.com>
2017-05-17 14:47:17 +0300

    create model 'post'

--- wall/models.py
+++ wall/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    caption = models.CharField(max_length=200)
+    content = models.CharField(max_length=2000)
+    image   = models.URLField()
+    pub_date = models.DateTimeField('date published')
--- wall/views.py
+++ wall/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def wall(request):
-    return render(request, 'wall/wall.html')
+    post = Post.objects.order_by('pub_date')
+    return render(request, 'wall/wall.html', {'posts': post})
